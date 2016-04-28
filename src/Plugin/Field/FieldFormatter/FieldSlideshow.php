0ae04d1436057c85dcb328ab4079017444bf3528
ganesan <ganesan.gopal@ameexusa.com>
2016-04-28 18:35:54 +0530

    Converted tpl to twig files

--- src/Plugin/Field/FieldFormatter/FieldSlideshow.php
+++ src/Plugin/Field/FieldFormatter/FieldSlideshow.php
@@ -82,6 +82,25 @@ class FieldSlideshow extends ImageFormatter {
       'title'   => t('Title text'),
       'alt'     => t('Alt text'),
     );
+    $element['slideshow_caption'] = array(
+      '#title'          => t('Caption'),
+      '#type'           => 'select',
+      '#default_value'  => $this->getSetting('slideshow_caption'),
+      '#empty_option'   => t('Nothing'),
+      '#options'        => $captions,
+    );
+    $element['slideshow_caption_link'] = array(
+      '#title'          => t('Caption link'),
+      '#type'           => 'select',
+      '#default_value'  => $this->getSetting('slideshow_caption_link'),
+      '#empty_option'   => t('Nothing'),
+      '#options'        => $links,
+      '#states' => array(
+        'invisible' => array(
+          ':input[name$="[settings_edit_form][settings][slideshow_caption]"]' => array('value' => ''),
+        ),
+      ),
+    );
     $element['slideshow_fx'] = array(
       '#title'          => t('Transition effect'),
       '#type'           => 'select',
@@ -147,6 +166,27 @@ class FieldSlideshow extends ImageFormatter {
       '#type'           => 'checkbox',
       '#default_value'  => $this->getSetting('slideshow_controls'),
     );
+    $element['slideshow_controls_pause'] = array(
+      '#title'          => t('Create play/pause button'),
+      '#type'           => 'checkbox',
+      '#default_value'  => $this->getSetting('slideshow_controls_pause'),
+      '#states' => array(
+        'visible' => array(
+          ':input[name$="[settings_edit_form][settings][slideshow_controls]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $element['slideshow_controls_position'] = array(
+      '#title'          => t('Prev/next controls position'),
+      '#type'           => 'select',
+      '#options'        => array('before' => 'Before', 'after' => 'After'),
+      '#default_value'  => $this->getSetting('slideshow_controls_position'),
+      '#states' => array(
+        'visible' => array(
+          ':input[name$="[settings_edit_form][settings][slideshow_controls]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
     $element['slideshow_pause'] = array(
       '#title'          => t('Pause on hover'),
       '#type'           => 'checkbox',
@@ -209,6 +249,7 @@ class FieldSlideshow extends ImageFormatter {
     else {
       $summary[] = t('Original image');
     }
+
     $link_types = array(
       'content'   => t('content'),
       'file'      => t('file'),
@@ -218,10 +259,60 @@ class FieldSlideshow extends ImageFormatter {
     $link_types_settings = $this->getSetting('slideshow_link');
     if (isset($link_types[$link_types_settings])) {
       $link_type_message = t('Link to: @link', array('@link' => $link_types[$link_types_settings]));
-      if ($this->getSetting('slideshow_link') == 'colorbox') {
+      /*if ($this->getSetting('slideshow_link') == 'colorbox') {
         
-      }
+      }*/
       $summary[] = $link_type_message;
+    }
+
+    $caption_types = array(
+      'title' => t('Title text'),
+      'alt'   => t('Alt text'),
+    );
+    // Display this setting only if there's a caption.
+    $caption_types_settings = $this->getSetting('slideshow_caption');
+    if (isset($caption_types[$caption_types_settings])) {
+      $caption_message = t('Caption: @caption', array('@caption' => $caption_types[$caption_types_settings]));
+      $link_types_settings = $this->getSetting('slideshow_caption_link');
+      if (isset($link_types[$link_types_settings])) $caption_message .= ' (' . t('Link to: @link', array('@link' => $link_types[$link_types_settings])) . ')';
+      $summary[] = $caption_message;
+    }
+
+    $summary[] = t('Transition effect: @effect', array('@effect' => $this->getSetting('slideshow_fx')));
+    $summary[] = t('Speed: @speed', array('@speed' => $this->getSetting('slideshow_speed')));
+    $summary[] = t('Timeout: @timeout', array('@timeout' => $this->getSetting('slideshow_timeout')));
+
+    $orders = array(
+      'reverse' => t('Reverse order'),
+      'random'  => t('Random order'),
+    );
+    $orders_settings = $this->getSetting('slideshow_order');
+    if (isset($orders[$orders_settings])) {
+      $summary[] = $orders[$orders_settings];
+    }
+    $pause_button_text = "";
+    $slideshow_controls_pause = $this->getSetting('slideshow_controls_pause');
+    $slideshow_controls = $this->getSetting('slideshow_controls');
+    $slideshow_pause = $this->getSetting('slideshow_pause');
+    $slideshow_start_on_hover = $this->getSetting('slideshow_start_on_hover');
+    if (isset($slideshow_controls_pause) && $slideshow_controls_pause ) $pause_button_text = " " . t("(with play/pause)");
+    if (isset($slideshow_controls) && $slideshow_controls) $summary[] = t('Create prev/next controls') . $pause_button_text;
+    if (isset($slideshow_pause) && $slideshow_pause) $summary[] = t('Pause on hover');
+    if (isset($slideshow_start_on_hover) && $slideshow_start_on_hover) $summary[] = t('Activate on hover');
+
+    switch ($this->getSetting('slideshow_pager')) {
+      case 'number':
+        $summary[] = t('Pager') . ': ' . t('Slide number');
+      break;
+      case 'image':
+        $pager_image_message = t('Pager') . ': ' . t('Image') . ' (';
+        if (isset($image_styles[$this->getSetting('slideshow_pager_image_style')])) {
+          $pager_image_message .= t('Image style: @style', array('@style' => $image_styles[$this->getSetting('slideshow_pager_image_style')]));
+        }
+        else $pager_image_message .= t('Original image');
+        $pager_image_message .= ')';
+        $summary[] = $pager_image_message;
+      break;
     }
 
     return $summary;
@@ -231,7 +322,10 @@ class FieldSlideshow extends ImageFormatter {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
+ public function viewElements(FieldItemListInterface $items, $langcode) {
+    static $slideshow_count;
+    $slideshow_count = (is_int($slideshow_count)) ? $slideshow_count + 1 : 1;
+
     $elements = array();
     $image_style_setting = $this->getSetting('image_style');
 
@@ -240,7 +334,9 @@ class FieldSlideshow extends ImageFormatter {
     if (!empty($image_style_setting)) {
       $image_style = entity_load('image_style', $image_style_setting);
     }
+    
     foreach ($items as $delta => $item) {
+      
       if ($item->entity) {
         $image_uri = $item->entity->getFileUri();
         // Get image style URL
@@ -255,9 +351,48 @@ class FieldSlideshow extends ImageFormatter {
         
       }
     }
+    $pager = array(
+      '#theme'                => 'field_slideshow_pager',
+      '#items'                => $items,
+      '#pager'                => $this->getSetting('slideshow_pager'),
+      '#pager_image_style'    => $this->getSetting('slideshow_pager_image_style'),
+      //'#carousel_image_style' => $this->getSetting('slideshow_carousel_image_style'),
+      '#slideshow_id'         => $slideshow_count,
+      //'#carousel_skin'        => $this->getSetting('slideshow_carousel_skin'),
+    );
+    $controls = array(
+      '#theme'                => 'field_slideshow_controls',
+      '#slideshow_id'         => $slideshow_count,
+      '#controls_pause'       => $this->getSetting('slideshow_controls_pause'),
+    );   
     $elements[$delta] = array(
-      '#theme' => 'field_slideshow',
-      '#items' => $image_element,
+      '#theme'                => 'field_slideshow',
+      '#items'                => $items,
+      '#image_style'          => $this->getSetting('slideshow_image_style'),
+      '#order'                => $this->getSetting('slideshow_order'),
+      '#controls'             => $this->getSetting('slideshow_controls') === 1 ? $controls : array(),
+      '#controls_position'    => $this->getSetting('slideshow_controls_position'),
+      '#pager'                => $this->getSetting('slideshow_pager') !== '' ? $pager : array(),
+      '#pager_position'       => $this->getSetting('slideshow_pager_position'),
+      '#entity'               => array(),
+      '#slideshow_id'         => $slideshow_count,
+      '#js_variables'         => array(
+        'fx'                   => $this->getSetting('slideshow_fx'),
+        'speed'                => $this->getSetting('slideshow_speed'),
+        'timeout'              => $this->getSetting('slideshow_timeout'),
+        'pause'                => $this->getSetting('slideshow_pause'),
+        'start_on_hover'       => $this->getSetting('slideshow_start_on_hover'),
+        // 'carousel_visible'     => $this->getSetting('slideshow_carousel_visible'),
+        // 'carousel_scroll'      => $this->getSetting('slideshow_carousel_scroll'),
+        // 'carousel_speed'       => $this->getSetting('slideshow_carousel_speed'),
+        // 'carousel_vertical'    => $this->getSetting('slideshow_carousel_vertical'),
+        // 'carousel_circular'    => $this->getSetting('slideshow_carousel_circular'),
+        // 'carousel_follow'      => $this->getSetting('slideshow_carousel_follow'),
+        // 'carousel_skin'        => $this->getSetting('slideshow_carousel_skin'),
+        // Need to access the following variables in js too
+        'pager'                => $this->getSetting('slideshow_pager'),
+        'controls'             => $this->getSetting('slideshow_controls') === 1 ? $controls : array(),
+      ),
     );
     return $elements;
   }
